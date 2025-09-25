04e84d88abc37092b0f46e4067249ba403ab8607
Adnaan26 <adnaanshaikrs4@gmail.com>
2025-09-25 10:31:11 +0200

    Finished the NPC movement and animation

--- Assets/Scripts/Waypoints/WaypointMover.cs
+++ Assets/Scripts/Waypoints/WaypointMover.cs
@@ -1,36 +1,67 @@
 using UnityEngine;
+using System.Collections;
 
 public class WaypointMover : MonoBehaviour
 {
     [SerializeField] private Waypoints waypoints;
-
     [SerializeField] private float moveSpeed = 5f;
-
     [SerializeField] private float distanceThreshold = 1f;
 
+    [Header("Idle Settings")]
+    [SerializeField] private float minIdleTime = 2f; // minimum time to idle
+    [SerializeField] private float maxIdleTime = 5f; // maximum time to idle
+    [SerializeField] private float minWalkTime = 3f; // minimum walking before idling again
+    [SerializeField] private float maxWalkTime = 8f; // maximum walking before idling again
+
     private Transform currentWaypoint;
-    //current waypoint object moving to 
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Animator animator;
+    private bool isIdle = false;
+
+    private void Start()
     {
-        //setting position to the first waypoint 
+        animator = GetComponentInChildren<Animator>();
+
+        // Set first waypoint
         currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
         transform.position = currentWaypoint.position;
-        
-        // setting next waypoint target 
+
+        // Set next waypoint
         currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
         transform.LookAt(currentWaypoint);
+
+        // Start random walk/idle cycle
+        StartCoroutine(RandomIdleRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (isIdle) return; // stop moving if idle
+
+        // Movement
         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+
         if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
         {
             currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
             transform.LookAt(currentWaypoint);
+        }
+    }
+
+    private IEnumerator RandomIdleRoutine()
+    {
+        while (true)
+        {
+            // Walk for random time
+            float walkTime = Random.Range(minWalkTime, maxWalkTime);
+            animator.SetBool("isWalking", true);
+            isIdle = false;
+            yield return new WaitForSeconds(walkTime);
+
+            // Idle for random time
+            float idleTime = Random.Range(minIdleTime, maxIdleTime);
+            animator.SetBool("isWalking", false);
+            isIdle = true;
+            yield return new WaitForSeconds(idleTime);
         }
     }
 }
